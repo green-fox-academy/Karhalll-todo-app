db952f769ffcbc8841cf16582026b78c39f88833
Martin Fučík <mar.fucik@gmail.com>
2020-07-09 20:17:00 +0200

    List tasks done

--- src/todo.java
+++ src/todo.java
@@ -1,3 +1,8 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class todo {
   public static void main(String[] args) {
     if (args.length == 0) {
@@ -6,7 +11,7 @@ public class todo {
       for (String s : args) {
         switch (s) {
           case "-l":
-            System.out.println("switch");
+            showTasks();
             break;
           case "-a":
             System.out.println("");
@@ -24,5 +29,18 @@ public class todo {
     System.out.println("\t -a   Adds a new task");
     System.out.println("\t -r   Removes an task");
     System.out.println("\t -c   Completes an task");
+  }
+
+  private static void showTasks() {
+    try {
+      Path filePath = Paths.get("assets/tasks.txt");
+      List<String> lines = Files.readAllLines(filePath);
+
+      for (int i = 0; i < lines.size(); i++) {
+        System.out.println((i+1) + " - " + lines.get(i));
+      }
+    } catch (Exception e) {
+      System.out.println("Could not read the tasks.txt file!");
+    }
   }
 }
