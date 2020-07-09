589b21bc10f4a5ad13fd985d78b095a16e769408
Martin Fučík <mar.fucik@gmail.com>
2020-07-09 20:20:07 +0200

    Empty list done

--- src/todo.java
+++ src/todo.java
@@ -36,8 +36,12 @@ public class todo {
       Path filePath = Paths.get("assets/tasks.txt");
       List<String> lines = Files.readAllLines(filePath);
 
-      for (int i = 0; i < lines.size(); i++) {
-        System.out.println((i+1) + " - " + lines.get(i));
+      if (lines.size() > 0) {
+        for (int i = 0; i < lines.size(); i++) {
+          System.out.println((i+1) + " - " + lines.get(i));
+        }
+      } else {
+        System.out.println("No todos for today! :)");
       }
     } catch (Exception e) {
       System.out.println("Could not read the tasks.txt file!");
